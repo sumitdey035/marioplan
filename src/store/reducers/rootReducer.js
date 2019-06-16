fe12bc40f22a77a87888d3204798ec951ad819d7
Sumit Dey <sumit@curvetomorrow.com.au>
2019-06-16 10:07:16 +0530

    Adding Dummy Data

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,8 @@
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
-import { combineReducer } from 'redux';
+import { combineReducers } from 'redux';
 
-const rootReducer = combineReducer({
+const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer
 })
