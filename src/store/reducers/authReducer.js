dac29d2d34e68fdf8908854b0b49e786bcd65cf3
Sumit Dey <sumit@curvetomorrow.com.au>
2019-06-21 20:09:42 +0530

    Logging Users In

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,6 +1,22 @@
-const initState = {}
+const initState = {
+  authError: null
+}
 
 const authReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'LOGIN_ERROR':
+      console.log('Login error');
+      return {
+        ...state,
+        authError: 'LOGIN_ERROR'
+      }
+    case 'LOGIN_SUCCESS':
+      console.log('Login success');
+      return {
+        ...state,
+        authError: null
+      }
+  }
   return state;
 }
 
