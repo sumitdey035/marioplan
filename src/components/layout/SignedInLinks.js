20dcbbb40c35187e559db082b5f8c522bda2ec1e
Sumit Dey <sumit@curvetomorrow.com.au>
2019-06-22 11:13:46 +0530

    User profile data

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -8,7 +8,7 @@ const SignedInLinks = (props) => {
     <ul className="right">
       <li><NavLink to='/CreateProject'>New Project</NavLink></li>
       <li><a onClick={props.signOut}>Logout</a></li>
-      <li><NavLink to='/' className='btn btn-floating pink lighten-1'>NN</NavLink></li>
+      <li><NavLink to='/' className='btn btn-floating pink lighten-1'>{props.profile.initials}</NavLink></li>
     </ul>
   )
 }
